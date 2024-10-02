e9a7fea6a79ec3024eb807b0d7d470a63cedfd3f
ksmooi <kaishianmooi@gmail.com>
2024-10-03 00:24:06 +0800

    initial database schema (mysql)

---
+++ host/mysql/init_scripts/002_insert_data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (username, email) VALUES ('user1', 'user1@example.com');
+INSERT INTO users (username, email) VALUES ('user2', 'user2@example.com');
+INSERT INTO users (username, email) VALUES ('user3', 'user3@example.com');
